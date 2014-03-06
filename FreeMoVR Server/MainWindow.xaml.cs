7de3bfd32e5bb8370f5bab954a8b3bc127d5b394
lunaraine <chynna.velasco@gmail.com>
2014-03-06 03:45:13 -0800

    Updated Alchemy Code. Created server but can't exactly tell. Code compiles and runs.

--- FreeMoVR Server/MainWindow.xaml.cs
+++ FreeMoVR Server/MainWindow.xaml.cs
@@ -12,8 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 using Alchemy;
 using Alchemy.Classes;
+using System.Net;
 
 
 namespace FreeMoVR_Server
@@ -28,23 +30,50 @@ namespace FreeMoVR_Server
         {
             InitializeComponent();
 
-            // instantiate a new server - acceptable port and IP range,
-            // and set up your methods.
+              // instantiate a new server - acceptable port and IP range,
+              // and set up your methods.
 
-            var aServer = new WebSocketServer(81, IPAddress.Any)
-            {
+              var aServer = new WebSocketServer(81, IPAddress.Any) {
                 OnReceive = OnReceive,
                 OnSend = OnSend,
                 OnConnect = OnConnect,
                 OnConnected = OnConnected,
                 OnDisconnect = OnDisconnect,
                 TimeOut = new TimeSpan(0, 5, 0)
-            };
+              };
 
-            aServer.Start();
-
-
+              aServer.Start();
         }
+
+
+        static void OnDisconnect(UserContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void OnConnect(UserContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void OnSend(UserContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void OnReceive(UserContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void OnConnected(UserContext context)
+        {
+            Console.WriteLine("Client Connection From : " +
+            context.ClientAddress.ToString());
+
+            //BLAH HOW TO DATA BIND THIS THANG.
+        }
+
 
     }
 }
