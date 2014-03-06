6437a8a310b61f8ef1819916307bc8c1c2c38211
lunaraine <chynna.velasco@gmail.com>
2014-03-06 01:16:36 -0800

    Added vJoy Page and Alchemy Websockets Feature

--- FreeMoVR Server/MainWindow.xaml.cs
+++ FreeMoVR Server/MainWindow.xaml.cs
@@ -12,17 +12,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Alchemy;
+using Alchemy.Classes;
+
 
 namespace FreeMoVR_Server
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            // instantiate a new server - acceptable port and IP range,
+            // and set up your methods.
+
+            var aServer = new WebSocketServer(81, IPAddress.Any)
+            {
+                OnReceive = OnReceive,
+                OnSend = OnSend,
+                OnConnect = OnConnect,
+                OnConnected = OnConnected,
+                OnDisconnect = OnDisconnect,
+                TimeOut = new TimeSpan(0, 5, 0)
+            };
+
+            aServer.Start();
+
+
         }
+
     }
 }
