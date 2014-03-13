fe47ad9c1334e403cd0450d6e444fd69f717acfd
lunaraine <chynna.velasco@gmail.com>
2014-03-13 03:15:04 -0700

    Edited Control.xaml to include content. No data bindings applied.

--- FreeMoVR Server3/MainWindowViewModel.cs
+++ FreeMoVR Server3/MainWindowViewModel.cs
@@ -8,13 +8,23 @@ namespace FreeMoVR_Server3
 {
     class MainWindowViewModel
     {
+        public vJoyFeeder vj;
+
+        public string manufacturer;
+        public string version;
+        public string product;
+        public string serial;
 
         public MainWindowViewModel()
         {
-            vJoyFeeder vj = new vJoyFeeder();
-
+            this.vj = new vJoyFeeder();
             // id is already inputted into the class, it is forced as 1.
             vj.acquire();
+
+            this.manufacturer = vj.getManufacturer();
+            this.version = vj.getVersion();
+            this.product = vj.getProduct();
+            this.serial = vj.getSerialNumber();
         }
     }
 }
