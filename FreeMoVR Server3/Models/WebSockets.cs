9fbf236dae2b37349b61d99e595367e8c629545c
Patrick Do <patdohere@gmail.com>
2014-03-13 10:34:15 -0700

    Created Simple Server, Started Websockets Project

---
+++ FreeMoVR Server3/Models/WebSockets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fleck;
+
+namespace FreeMoVR_Server3.Models
+{
+    public class WebSockets
+    {
+        public WebSockets()
+        {
+            Fleck
+        }
+    }
+}
